024d54b7c481671a7f2eb3ebdd22b967174cef4a
factism001 <factism001@gmail.com>
2023-05-26 11:02:01 +0000

    solved

--- models/base_model.py
+++ models/base_model.py
@@ -68,7 +68,10 @@ class BaseModel:
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
-        return new_dict
+        if getenv("HBNB_TYPE_STORAGE") == "db":
+            if 'password' in new_dict:
+                del new_dict["password"]
+        return new_dictreturn new_dict
 
     def delete(self):
         """delete the current instance from the storage"""
